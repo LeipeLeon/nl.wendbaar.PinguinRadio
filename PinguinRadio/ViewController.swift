2f0b7222a76c64689976d674fcdc172b2149dd08
Leon Berenschot <leipeleon@gmail.com>
2015-12-19 16:20:05 +0100

    Initial Commit

---
+++ PinguinRadio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PinguinRadio
+//
+//  Created by Leon Berenschot on 19/12/15.
+//  Copyright © 2015 wendbaar.nl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
