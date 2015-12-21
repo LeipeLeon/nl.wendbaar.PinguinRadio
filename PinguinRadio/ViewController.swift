cc37fef332d5ed196fe93b2a8320c171101cf0b3
Leon Berenschot <leipeleon@gmail.com>
2015-12-21 21:19:34 +0100

    Use storyboard and a viewcontroller

---
+++ PinguinRadio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  boilerplate
+//
+//  Created by Leon Berenschot on 21/12/15.
+//  Copyright © 2015 Ticketpunk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
