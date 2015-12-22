d6fc201ec1f24233a9ad13c475acf5f8bd28309e
Leon Berenschot <leipeleon@gmail.com>
2015-12-22 02:23:58 +0100

    add HiQuality images

--- PinguinRadio/PlayerViewController.swift
+++ PinguinRadio/PlayerViewController.swift
@@ -11,7 +11,7 @@ import AVKit
 
 class PlayerViewController: AVPlayerViewController {
 
-    var station: Station?
+    var station: Station!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +20,7 @@ class PlayerViewController: AVPlayerViewController {
         let bounds = UIScreen.mainScreen().bounds.size
 
         let image = UIImage(named: station!.logo_url)
-        let margin = CGFloat(100)
+        let margin = CGFloat(62)
         let newHeight = (image!.size.height / image!.size.width) * (bounds.width - margin - margin)
         
         let imageView = UIImageView(frame: CGRectMake(margin, margin, bounds.width - margin - margin, newHeight))
@@ -28,11 +28,8 @@ class PlayerViewController: AVPlayerViewController {
         imageView.image = image
 
         imageView.contentMode = UIViewContentMode.ScaleAspectFit
-        imageView.backgroundColor = UIColor(red:0.13, green:0.11, blue:0.35, alpha:1)
-        
         
         let overlayView = UIView(frame: CGRectMake(0, 0, bounds.width, bounds.height))
-        overlayView.backgroundColor = UIColor(red:0.13, green:0.11, blue:0.35, alpha:1)
         overlayView.addSubview(imageView)
 
         contentOverlayView?.addSubview(overlayView)
